2bf9a308838d9dcfe4c9331d8d86d3f75ae79334
David Spindler <ashleywoods@Ashleys-MacBook-Pro.local>
2021-07-29 12:57:56 -0700

    first commit

---
+++ doggos.js
@@ -0,0 +1,25 @@
+
+
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const promise = fetch(DOG_URL);
+const doggos = document.querySelector(".doggos");
+
+
+
+function addNewDoggo() {
+  const promise = fetch(DOG_URL);
+  promise
+    .then(function(response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function(processedResponse) {
+      const img = document.createElement("img");
+      img.src = processedResponse.message;
+      img.alt = "Cute doggo";
+      doggos.appendChild(img);
+    });
+}
+addNewDoggo();
+document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
